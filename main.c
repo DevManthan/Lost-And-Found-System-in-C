85da2871efa9c5b938024daa7b5ad53a3635e533
Manthan <manthandhiman2007@gmail.com>
2026-03-15 15:28:07 +0530

    FIrst Commit, Initialize Repo

---
+++ main.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+
+    int ch;
+
+    while(1){
+        printf("Enter your Choice : \n");
+        printf("1. Report Lost item \n");
+        printf("2. Check for lost item \n");
+        printf("3. Check found Records\n");
+        printf("3. Check lost Records\n");
+        printf("4. Exit\n");
+        scanf("%d", &ch);
+
+        switch (ch)
+        {
+        case 1:
+            printf("Reporting\n");
+            break;
+        case 2:
+            printf("checking\n");
+            break;
+        case 3:
+            printf("records\n");
+            break;
+        case 4:
+            printf("exit\n");
+            exit(0);
+            break;
+        default:
+            printf("******INVALID CHOICE*******\n");
+            break;
+        }
+    }
+}
